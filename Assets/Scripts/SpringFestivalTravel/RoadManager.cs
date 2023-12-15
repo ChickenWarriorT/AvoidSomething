8a9e625e8127a5b5d9a770e84dc722d08933f9af
xiangyicheng <xiangyicheng@forfungame.com>
2023-12-15 19:06:03 +0800

    随机车道数量

--- Assets/Scripts/SpringFestivalTravel/RoadManager.cs
+++ Assets/Scripts/SpringFestivalTravel/RoadManager.cs
@@ -5,7 +5,7 @@ public class RoadManager : MonoBehaviour
 {
     public GameObject roadPrefab; // 用来放置车道的Prefab
     public GameObject boundary; // 用来装所有车道的AreaPrefab
-    public int numberOfRoads; // 您希望的车道数
+    public int initNumberOfRoads; // 您希望的车道数
     public int numberOfStartGroups;
     public int numberOfGroups;
     public float spaceBetweenRoads; // 车道之间的空隙宽度
@@ -13,20 +13,29 @@ public class RoadManager : MonoBehaviour
     public Transform roadGroupsTransform;
     public List<GameObject> roads;
     public List<GameObject> roadGroups;
-
+    private float roadWidth;
+    private float roadLength;
     void Start()
     {
         // 获取AreaPrefab的宽度
         float areaWidth = boundary.GetComponent<SpriteRenderer>().bounds.size.x;
         float areaLength = boundary.GetComponent<SpriteRenderer>().bounds.size.y;
-
+        roadLength = areaLength;
 
         // 计算每个车道的宽度
-        float laneWidth = CalculateRoadWidth(areaWidth, numberOfRoads, spaceBetweenRoads);
+        roadWidth = CalculateRoadWidth(areaWidth, initNumberOfRoads, spaceBetweenRoads);
 
+        
+        // 初始生成车道组
+        GenerateRoadGroups(roadWidth, roadLength, numberOfStartGroups);
+    }
 
-        // 生成车道组
-        GenerateRoadGroups(laneWidth, areaLength, numberOfStartGroups);
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GenerateRoadGroup(roadWidth, roadLength, 1);
+        }
     }
 
     float CalculateRoadWidth(float totalWidth, int numRoads, float spaceBetween)
@@ -34,26 +43,34 @@ public class RoadManager : MonoBehaviour
         return (totalWidth - (numRoads - 1) * spaceBetween) / numRoads;
     }
 
+    //生成一个道路组
+    GameObject GenerateRoadGroup(float roadWidth, float roadLength, int numberOfRoad)
+    {
+       
+        GameObject roadGroup = new GameObject("RoadGroup");
+        roadGroup.transform.parent = roadGroupsTransform;
+        roadGroup.AddComponent<Rigidbody2D>().gravityScale = 0;
+
+        roadGroups.Add(roadGroup);
+        GenerateLanes(roadWidth, roadLength, numberOfRoad, roadGroup.transform);
+        return roadGroup;
+    }
+
     void GenerateRoadGroups(float roadWidth, float roadLength, int numberOfGroups)
     {
         for (int i = 0; i < numberOfGroups; i++)
         {
-            GameObject roadGroup = new GameObject("RoadGroup_" + i);
-            roadGroup.transform.parent = roadGroupsTransform;
-            roadGroup.AddComponent<Rigidbody2D>().gravityScale = 0;
+            GameObject roadGroup = GenerateRoadGroup(roadWidth, roadLength, Random.Range(1, 5));
             float yPos = i * (roadLength + groupVerticalOffset);
             roadGroup.transform.position = new Vector3(roadGroup.transform.position.x, yPos);
-
-            roadGroups.Add(roadGroup);
-            GenerateLanes(roadWidth, roadLength, roadGroup.transform);
         }
     }
-    void GenerateLanes(float roadWidth, float roadLength, Transform group)
+    void GenerateLanes(float roadWidth, float roadLength,int numberOfRoad, Transform group)
     {
         Vector3 startPosition = boundary.transform.position - new Vector3(boundary.GetComponent<SpriteRenderer>().bounds.size.x / 2, 0, 0);
         float space = roadWidth + spaceBetweenRoads;
 
-        for (int i = 0; i < numberOfRoads; i++)
+        for (int i = 0; i < numberOfRoad; i++)
         {
             // 计算当前车道的位置
             Vector3 lanePosition = startPosition + new Vector3(space * i + roadWidth / 2, group.position.y, 0);
