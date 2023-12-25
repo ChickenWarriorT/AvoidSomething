9b8a7c3c5dbc49f38e846214f92431b274ba3a7b
DemoNinja <775819329@qq.com>
2023-12-25 22:21:10 +0800

    修改为线条分割道路

--- Assets/Scripts/SpringFestivalTravel/RoadManager.cs
+++ Assets/Scripts/SpringFestivalTravel/RoadManager.cs
@@ -8,10 +8,11 @@ public class RoadManager : MonoBehaviour
     public GameObject solidLinePrefab;
     public GameObject dashedLinePrefab;
 
+    
     public int initNumberOfRoads; // 您希望的车道数
     public int numberOfStartGroups;
     public int numberOfGroups;
-    public float lineWidth; //线的宽度
+    //public float lineWidth; //线的宽度
     public float groupVerticalOffset;
     public Transform roadGroupsTransform;
     public List<GameObject> roads;
@@ -27,13 +28,14 @@ public class RoadManager : MonoBehaviour
         // 获取AreaPrefab的宽度
         roadGroupWidth = boundary.GetComponent<SpriteRenderer>().bounds.size.x;
         float roadGroupLength = boundary.GetComponent<SpriteRenderer>().bounds.size.y;
+        Debug.Log(roadGroupLength);
         roadLength = roadGroupLength;
 
         // 计算每个车道的宽度
-        roadWidth = CalculateRoadWidth(roadGroupWidth, initNumberOfRoads, lineWidth);
+        roadWidth = CalculateRoadWidth(roadGroupWidth, initNumberOfRoads);
 
         // 初始生成车道组
-        GenerateRoadGroups(roadWidth, roadLength, numberOfStartGroups, 6);
+        GenerateRoadGroups(roadWidth, roadLength, numberOfStartGroups, initNumberOfRoads);
         distanceForNewGroup = distanceGap;
     }
 
@@ -61,9 +63,9 @@ public class RoadManager : MonoBehaviour
     }
 
     //计算道路宽度
-    float CalculateRoadWidth(float totalWidth, int numRoads, float spaceBetween)
+    float CalculateRoadWidth(float totalWidth, int numRoads)
     {
-        return (totalWidth - numRoads * spaceBetween) / numRoads;
+        return totalWidth / numRoads;
     }
 
     /// <summary>
@@ -83,73 +85,38 @@ public class RoadManager : MonoBehaviour
             roadGroup.AddComponent<Rigidbody2D>().gravityScale = 0;
 
             roadGroups.Add(roadGroup);
-            GenerateLane(roadWidth, roadLength, numberOfRoad, roadGroup.transform);
-            //GenerateLanes(roadWidth, roadLength, numberOfRoad, roadGroup.transform);
+            GenerateLines(roadWidth, roadLength, numberOfRoad, roadGroup.transform);
             roadGroup.transform.position = new Vector3(roadGroup.transform.position.x, yPos + i * (roadLength + groupVerticalOffset), 0);
         }
     }
 
-
-
-
-    void GenerateLanes(float roadWidth, float roadLength, int numberOfRoad, Transform group)
+    void GenerateLines(float roadWidth, float roadLength, int numberOfRoad, Transform group)
     {
-        // 计算所有道路的总宽度（包括空隙）
-        float totalRoadsWidth = numberOfRoad * roadWidth + (numberOfRoad - 1) * lineWidth;
-        // 计算第一条道路的起始位置，使道路组居中
-        float startOffsetX = (boundary.GetComponent<SpriteRenderer>().bounds.size.x - totalRoadsWidth) / 2;
-        Vector3 startPosition = new Vector3(startOffsetX, 0, 0) + boundary.transform.position
-                                - new Vector3(boundary.GetComponent<SpriteRenderer>().bounds.size.x / 2, 0, 0);
-        float space = roadWidth + lineWidth;
-
-        for (int i = 0; i < numberOfRoad; i++)
-        {
-            // 计算当前车道的位置
-            Vector3 lanePosition = startPosition + new Vector3(space * i + roadWidth / 2, group.position.y, 0);
-
-            // 实例化RoadPrefab
-            GameObject road = Instantiate(roadPrefab, lanePosition, Quaternion.identity, group);
-
-            road.transform.localScale = new Vector3(roadWidth, roadLength, 1);
-            roads.Add(road);
-        }
-    }
-    void GenerateLane(float roadWidth, float roadLength, int numberOfRoad, Transform group)
-    {
-        //float originalLineWidth = GetOriginalLineWidth(linePrefab); // 获取原始宽度
 
         // 计算第一条线的起始位置
-        float startOffsetX = (boundary.GetComponent<SpriteRenderer>().bounds.size.x - roadGroupWidth) ;
         Vector3 startPosition = boundary.transform.position
-                            - new Vector3(boundary.GetComponent<SpriteRenderer>().bounds.size.x / 2, 0, 0);
-
-        float space = roadWidth + lineWidth;
+                                - new Vector3(boundary.GetComponent<SpriteRenderer>().bounds.size.x / 2, 0, 0);
+        Debug.Log("Generating lines, Number of Roads: " + numberOfRoad);
         for (int i = 0; i <= numberOfRoad; i++)
         {
             GameObject linePrefab = (i == 0 || i == numberOfRoad) ? solidLinePrefab : dashedLinePrefab;
+            if (linePrefab == null)
+            {
+                Debug.LogError("Line prefab is null for index: " + i);
+                continue;
+            }
             GameObject line = Instantiate(linePrefab, group);
 
-            // 计算当前车道的位置
-            Vector3 linePosition = startPosition + new Vector3(space * i + roadWidth / 2, group.position.y, 0);
-            // 计算分割线的位置
+            float scaleY = roadLength / line.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+
+            // 计算当前分割线的位置
+            Vector3 linePosition = startPosition + new Vector3(i * roadWidth, 0, 0);
             line.transform.localPosition = linePosition;
 
-            // 可能需要调整分割线的缩放以适应道路宽度和长度
-            line.transform.localScale = new Vector3(lineWidth, roadLength, 0);
+            // 应用缩放
+            line.transform.localScale = new Vector3(line.transform.localScale.x, scaleY, 1);
         }
     }
-    private float GetOriginalLineWidth(GameObject linePrefab)
-    {
-        SpriteRenderer renderer = linePrefab.GetComponent<SpriteRenderer>();
-        if (renderer != null && renderer.sprite != null)
-        {
-            return renderer.sprite.bounds.size.x; // 返回原始宽度
-        }
-        return 0f;
-    }
-
-
-
 
     private void FixedUpdate()
     {
